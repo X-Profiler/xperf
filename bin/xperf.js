c9499df6aa4718ea33078f1a240d1a7533408547
hyj1991 <yeekwanvong@gmail.com>
2021-07-06 19:48:22 +0800

    xargs: supoport boolean options

--- bin/xperf.js
+++ bin/xperf.js
@@ -7,6 +7,9 @@ const xargs = require('../lib/xargs');
 const args = xargs
   .usage('xperf start [script.js] [options]')
   .command('start', script => (/^.*\.js$/.test(script)), 'script must be js!')
+  .option('start-cpu-profiling').boolean()
+  .option('start-gc-profiling').boolean()
+  .option('start-heap-profling').boolean()
   .argv;
 
 console.log(12333, args);
